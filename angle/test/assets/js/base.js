205555e4880ace59155c830ee25d01b98a888fc4
e <sunny_qqmio@sina.com>
2017-04-12 11:12:36 +0800

    n

--- angle/test/assets/js/base.js
+++ angle/test/assets/js/base.js
@@ -1,7 +1,20 @@
- ;(function(win, doc){
-        function change(){
-            doc.documentElement.style.fontSize=20*doc.documentElement.clientWidth/375+'px';
-        }
-        change();
-        win.addEventListener('resize', change, false);
-    })(window, document);
+ (function (doc, win) {
+    var docEl = doc.documentElement,
+            resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+            recalc = function () {
+                var clientWidth = docEl.clientWidth;
+                console.log(clientWidth);
+                if (!clientWidth)
+                    return;
+                if(clientWidth>=1024){
+                    docEl.style.fontSize = 20+'px';
+                }else if(clientWidth>=768){
+                    docEl.style.fontSize = 20  + 'px';
+                }else{
+                    docEl.style.fontSize = 40 * (clientWidth / 750) + 'px';
+                }
+            };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
